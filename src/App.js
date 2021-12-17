0b1aa6de54f1c9e041efb790fca7aa00cd501a3e
ukazdal <ibrahimufukkazdal@gmail.com>
2021-12-17 16:25:06 +0300

    Card-Home-Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact />
+          <Route path="/" exact component={Home} />
         </Switch>
       </Router>
     </>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../App.css";
+import Cards from "../Cards";
+import HeroSection from "../HeroSection";
+import Cards2 from "../Cards2";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      {/* <Cards2 /> */}
+    </>
+  );
+}
+
+export default Home;
